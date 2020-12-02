1c51454b123feed400d28810dcd2c254d953362a
BuildTools <unconfigured@null.spigotmc.org>
2020-12-02 14:07:44 +0100

    Initial commit

---
+++ src/main/java/de/mrln/ownlanguagemod/client/OwnLanguageModClient.java
@@ -0,0 +1,14 @@
+package de.mrln.ownlanguagemod.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class OwnLanguageModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+        
+    }
+}
